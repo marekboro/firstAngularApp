e77604cccbd2fba003a401053d2f8cf558213c78
marekboro <marek.borowicz83@gmail.com>
2021-01-21 17:49:24 +0000

    added rating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   // templateUrl: './app.component.html',
-  template: `<h1>{{title}} --- - - --- - ->  {{newData}}</h1> <products> </products> `,
+  template:`
+            <h1>
+              {{title}} --- - - --- - ->  {{newData}}
+            </h1> 
+            <products> </products>
+            <img src="{{imageUrl}}"/>
+            <button class="btn btn-primary"> Example </button>
+            <button (click)="onClickMe($event)" class="btn btn-primary"> Toggle on/off btn</button>
+            <button class="btn btn-primary" [class.disabled]=!isValid> ON/OFF </button>
+            <rating> </rating>
+            <h3> ds </h3>
+            `,
   // providers:[ProductService]
 
 })
 export class AppComponent {
   title:string = '1st AngularApp';
   newData = '2nd variable passed in';
+  imageUrl = 'https://www.w3schools.com/html/pic_mountain.jpg';
+  isValid = false;
+
+  onClickMe($event){
+    this.isValid = !this.isValid;
+    console.log("Clicked toggle",$event)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products.components';
-import {ProductService} from './product.service'
+import { ProductService } from './product.service'
+import { StarComponent } from './star.component'
+import { StarfillComponent } from './starfill.component'
+import { RatingComponent } from './rating.component'
+
+
 
 @NgModule({
   declarations: [
-    AppComponent,ProductsComponent
+    AppComponent,ProductsComponent,StarComponent,StarfillComponent,RatingComponent
   ],
   imports: [
     BrowserModule,
